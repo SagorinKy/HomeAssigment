76a03839b25396203151513b8530157a63c68e8e
Kirill Sagorin <st128530@student.spbu.ru>
2024-09-20 02:51:14 +0300

    Добавил реалиацию функций вывода

---
+++ utiltity.cpp
@@ -0,0 +1,17 @@
+/* Kirill Sagorin st128530@student.spbu.ru
+first test project */
+
+#include <iostream>
+#include "utiltity.h"
+
+using namespace std;
+
+void printhell()
+{
+	cout << "hellO, world!" << endl;
+}
+
+void printhell(string name)
+{
+	cout << "hello, " << name << " !" << endl;
+}
