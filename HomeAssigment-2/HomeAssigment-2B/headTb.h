437399dd6daa369ff6bc2be76d8d00242875be09
Kirill Sagorin <st128530@student.spbu.ru>
2024-10-11 02:23:32 +0300

    add header file

---
+++ HomeAssigment-2/HomeAssigment-2B/headTb.h
@@ -0,0 +1,15 @@
+#include <filesystem>
+#include <string>
+#include <fstream>
+
+int FileSize(std::string name);
+
+int CountInt(char* arr);
+
+int GetInt(char* arr, int num);
+
+void InsertSring(char* arr);
+
+char* ReadFile(std::string name);
+
+
