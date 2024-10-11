6cb36da7a85ea42ce94829af533a27d5af1a2de1
Kirill Sagorin <st128530@student.spbu.ru>
2024-10-11 03:26:45 +0300

    wrote function to calculate RPN, changed func GetInt o Please enter the commit message for your changes. Lines starting

--- HomeAssigment-2/HomeAssigment-2B/headTb.cpp
+++ HomeAssigment-2/HomeAssigment-2B/headTb.cpp
@@ -32,15 +32,56 @@ int CountInt(char* arr, int n)
 	return k;
 }
 
-int GetInt(char* arr, int num)
+int GetInt(char* arr, int* num)
 {
 	int k = 0;
-	while(arr[num] != ' ')
+	while(arr[*num] != ' ')
 	{
 		k *= 10;
-		k += arr[num];
-		num++;
+		k += (int)arr[*num];
+		*num++;
 	}
 	return k;
+}
+
+int Poland(char* arr, int size)
+{
+	int n = CountInt(arr, size);
+	int* stek = new int(n);
+	int stekind = 0;
+	for(int i = 0; i < size; i++)
+	{
+		if(arr[i] != '*' || arr[i] != '/' || arr[i] != '+' || arr[i] != '-' || arr[i] != ' ')
+		{
+			stek[stekind] = GetInt(arr, &i);
+			stekind++;
+		}
+		else if(arr[i] != ' ')
+		{
+			if(arr[i] = '*')
+			{
+				arr[i - 2] *= arr[i - 1];
+				arr[i - 1] = 0;
+				stekind--;
+			}
+			if(arr[i] = '/')
+                        {
+                                arr[i - 2] /= arr[i - 1];
+                                arr[i - 1] = 0;
+                                stekind--;
+                        } if(arr[i] = '+')
+                        {
+                                arr[i - 2] += arr[i - 1];
+                                arr[i - 1] = 0;
+                                stekind--;
+                        } if(arr[i] = '-')
+                        {
+                                arr[i - 2] -= arr[i - 1];
+                                arr[i - 1] = 0;
+                                stekind--;
+                        }
+		}
+	}
+	return stek[0];
 }
 		
