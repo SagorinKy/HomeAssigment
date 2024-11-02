fdd58b103e25b97d333b3e1ea1e0aafc72c773f3
Kirill Sagorin <st128530@student.spbu.ru>
2024-11-02 10:42:33 +0300

    gtest of get/set methods

---
+++ HomeAssigment-3/test-Autobot.cpp
@@ -0,0 +1,29 @@
+#include "AutobotClass.h"
+#include "gtest/gtest.h"
+
+TEST(Autobot, getRating) {
+    Autobot a; 
+    EXPECT_EQ(a.getRating(), 100);
+}
+
+TEST(Autobot, setRating) {
+    Autobot a;
+    a.setRating(80);
+    EXPECT_EQ(a.getRating(), 80);
+}
+
+TEST(Autobot, getColour) {
+    Autobot a; 
+    EXPECT_EQ(a.getColour(), "red");
+}
+
+TEST(Autobot, setColour) {
+    Autobot a; 
+    a.setColour("blue");
+    EXPECT_EQ(a.getColour(), "blue");
+}
+
+TEST(Autobot, sleep) {
+    Autobot a; 
+    EXPECT_TRUE(a.sleep()); 
+}
