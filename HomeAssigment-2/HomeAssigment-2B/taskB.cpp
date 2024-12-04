de609aa84abe7a2f55a8984f245cc5ec741a83e4
Kirill Sagorin <st128530@student.spbu.ru>
2024-12-04 23:10:42 +0300

    finally fixed segmentation error :')

--- HomeAssigment-2/HomeAssigment-2B/taskB.cpp
+++ HomeAssigment-2/HomeAssigment-2B/taskB.cpp
@@ -4,11 +4,17 @@
 int main()
 {
 	std::string name = "poland.txt";
+//	std::cout<<0<<std::endl;
 	int size = FileSize(name);
-	char* arr = new char[size];
-//std::cout<<0;
+	std::cout<<size<<std::endl;
+	char* arr;
+	std::cout<<0<<std::endl;
 	arr = ReadFile(name);
-//std::cout<<1;
+//	std::cout<<"jjjjjj"<< std::endl;
+	for(int i = 0; i < size; i++)
+		std::cout<<arr[i];
+	std::cout<<std::endl;
+	std::cout<<"poland() - begins"<<std::endl;
 	int result = Poland(arr, size);
 //std::cout<<2<<std::endl;
 	std::cout << result << std::endl;
