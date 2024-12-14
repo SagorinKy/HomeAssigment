2f3aec99dda0e4d3d104dfe16344f00d7eec28f1
Kirill Sagorin <st128530@student.spbu.ru>
2024-12-15 02:56:59 +0300

    class 1-3 for tests realization

---
+++ HomeAssigment-6/classes.cpp
@@ -0,0 +1,51 @@
+#include "classes.h"
+#include <iostream>
+
+bool Class1::bar(int a, std::vector<float>& vec)
+{
+	return a > 0;
+}
+bool Class2::bar(int a, std::vector<float>& vec)
+{
+	return !vec.empty();
+}
+bool Class3::bar(int a, std::vector<float>& vec)
+{
+	return vec.size() == -a;
+}
+int Class1::c_1_1()
+{
+	return 1;
+}
+float Class1::c_1_2()
+{
+	return 1.0 / 3 * 3;
+}
+void Class1::c_1_3()
+{
+	std::cout << "class 1" << std::endl;
+}
+int Class2::c_2_1()
+{
+	return 2;
+}
+float Class2::c_2_2()
+{
+	return 2.0 / 3 * 3;
+}
+void Class2::c_2_3()
+{
+	std::cout << "class 2" << std::endl;
+}
+int Class3::c_3_1()
+{
+	return 3;
+}
+float Class3::c_3_2()
+{
+	return 3.0 / 3 * 3;
+}
+void Class3::c_3_3()
+{
+	std::cout << "class 3" << std::endl;
+}
---
+++ HomeAssigment-6/classes.h
@@ -0,0 +1,28 @@
+#include <vector>
+
+class Class1
+{
+public:
+	bool bar(int a, std::vector<float>& vec);
+	int c_1_1();
+	float c_1_2();
+	void c_1_3();
+};
+
+class Class2
+{
+public:
+	bool bar(int a, std::vector<float>& vec);
+	int c_2_1();
+	float c_2_2();
+	void c_2_3();
+};
+
+class Class3
+{
+public:
+	bool bar(int a, std::vector<float>& vec);
+	int c_3_1();
+	float c_3_2();
+	void c_3_3();
+};
