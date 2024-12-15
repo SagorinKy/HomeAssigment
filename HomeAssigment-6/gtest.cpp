140dd74373c0cf76df367ba42b41ee096a3b34d2
Kirill Sagorin <st128530@student.spbu.ru>
2024-12-15 05:14:10 +0300

    tests now working

--- HomeAssigment-6/gtest.cpp
+++ HomeAssigment-6/gtest.cpp
@@ -3,7 +3,7 @@
 #include "gtest/gtest.h"
 #include <vector>
 
-TEST(TemplClass, foo)
+TEST(TemplClass, foo_true)
 {
 	int n = 1;
 	std::vector<float> vec(5);
@@ -13,7 +13,7 @@ TEST(TemplClass, foo)
 	
 	EXPECT_TRUE(a.foo());
 }
-TEST(TemplClass(float), foo)
+TEST(TemplClass, foo_false)
 {	
 	int n = 1;
 	std::vector<float> vec(5);
@@ -23,5 +23,55 @@ TEST(TemplClass(float), foo)
 	
 	EXPECT_FALSE(b.foo());
 }
+TEST(Class1, foo_true)
+{
+	int n = 1;
+	std::vector<float> vec(5);
+
+	Class1 a = Class1();
+	EXPECT_TRUE(a.bar(n, vec));
+}
+TEST(Class1, foo_false)
+{
+	int n = 0;
+	std::vector<float> vec(5);
+
+	Class1 a = Class1();
+	EXPECT_FALSE(a.bar(n, vec));
+}
 
 
+TEST(Class2, foo_true)
+{
+	int n = 1;
+	std::vector<float> vec(5);
+
+	Class2 a = Class2();
+	EXPECT_TRUE(a.bar(n, vec));
+}
+TEST(Class2, foo_false)
+{
+	int n = 1;
+	std::vector<float> vec(0);
+
+	Class2 a = Class2();
+	EXPECT_FALSE(a.bar(n, vec));
+}
+
+
+TEST(Class3, foo_true)
+{
+	int n = -5;
+	std::vector<float> vec(5);
+
+	Class3 a = Class3();
+	EXPECT_TRUE(a.bar(n, vec));
+}
+TEST(Class3, foo_false)
+{
+	int n = 1;
+	std::vector<float> vec(5);
+
+	Class3 a = Class3();
+	EXPECT_FALSE(a.bar(n, vec));
+}
