24ef2c52d20f4db08be7d3c5da1e01c8c4a8f291
Kirill Sagorin <st128530@student.spbu.ru>
2024-12-20 08:25:26 +0300

    added astyle

--- HomeAssigment-6/classes.h
+++ HomeAssigment-6/classes.h
@@ -3,26 +3,29 @@
 class Class1
 {
 public:
-	bool bar(int a, std::vector<float>& vec);
-	int c_1_1();
-	float c_1_2();
-	void c_1_3();
+    Class1() {}
+    bool bar(int a, std::vector<float>& vec);
+    int c_1_1();
+    float c_1_2();
+    void c_1_3();
 };
 
 class Class2
 {
 public:
-	bool bar(int a, std::vector<float>& vec);
-	int c_2_1();
-	float c_2_2();
-	void c_2_3();
+    Class2() {}
+    bool bar(int a, std::vector<float>& vec);
+    int c_2_1();
+    float c_2_2();
+    void c_2_3();
 };
 
 class Class3
 {
 public:
-	bool bar(int a, std::vector<float>& vec);
-	int c_3_1();
-	float c_3_2();
-	void c_3_3();
+    Class3() {}
+    bool bar(int a, std::vector<float>& vec);
+    int c_3_1();
+    float c_3_2();
+    void c_3_3();
 };
