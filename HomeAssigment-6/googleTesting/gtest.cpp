e13c237fad5c4ec0f2c5726fa221822a2ea058e6
Kirill Sagorin <st128530@student.spbu.ru>
2025-03-07 02:21:01 +0300

    made tests as requared (didn't understand task before)

--- HomeAssigment-6/googleTesting/gtest.cpp
+++ HomeAssigment-6/googleTesting/gtest.cpp
@@ -3,75 +3,86 @@
 #include "gtest/gtest.h"
 #include <vector>
 
-TEST(TemplClass, foo_true)
+TEST(TemplClass, foo_int_true)
 {
 	int n = 1;
 	std::vector<float> vec(5);
 
 	int a_test = 1;
-	TemplClass a(a_test, n, vec);
+	TemplClass<int> a(a_test, n, vec);
 	
 	EXPECT_TRUE(a.foo());
 }
-TEST(TemplClass, foo_false)
+TEST(TemplClass, foo_false_false)
 {	
 	int n = 1;
 	std::vector<float> vec(5);
 
 	float b_test = 5.5;
-	TemplClass b(b_test, n, vec);
+	TemplClass<float> b(b_test, n, vec);
 	
 	EXPECT_FALSE(b.foo());
 }
-TEST(Class1, foo_true)
+TEST(TemplClass, foo_Class1_true)
 {
 	int n = 1;
 	std::vector<float> vec(5);
 
 	Class1 a = Class1();
-	EXPECT_TRUE(a.bar(n, vec));
+	TemplClass<Class1> cl(a, n, vec);
+
+	EXPECT_TRUE(cl.foo());
 }
-TEST(Class1, foo_false)
+TEST(TemplClass, foo_Class1_false)
 {
 	int n = 0;
 	std::vector<float> vec(5);
-
+	
 	Class1 a = Class1();
-	EXPECT_FALSE(a.bar(n, vec));
+	TemplClass<Class1> cl(a, n, vec);
+	
+	EXPECT_FALSE(cl.foo());
 }
 
 
-TEST(Class2, foo_true)
+TEST(TemplClass, foo_Class2_true)
 {
 	int n = 1;
 	std::vector<float> vec(5);
 
 	Class2 a = Class2();
-	EXPECT_TRUE(a.bar(n, vec));
+	TemplClass<Class2> cl(a, n, vec);
+	EXPECT_TRUE(cl.foo());
 }
-TEST(Class2, foo_false)
+TEST(TemplClass, foo_Class2_false)
 {
 	int n = 1;
 	std::vector<float> vec(0);
 
 	Class2 a = Class2();
-	EXPECT_FALSE(a.bar(n, vec));
+	TemplClass<Class2> cl(a, n, vec);
+
+	EXPECT_FALSE(cl.foo());
 }
 
 
-TEST(Class3, foo_true)
+TEST(TemplClass, foo_Class3_true)
 {
 	int n = -5;
 	std::vector<float> vec(5);
 
 	Class3 a = Class3();
-	EXPECT_TRUE(a.bar(n, vec));
+	TemplClass<Class3> cl(a, n, vec);
+
+	EXPECT_TRUE(cl.foo());
 }
-TEST(Class3, foo_false)
+TEST(TemplClass, foo_Class3_false)
 {
 	int n = 1;
 	std::vector<float> vec(5);
 
 	Class3 a = Class3();
-	EXPECT_FALSE(a.bar(n, vec));
+	TemplClass<Class3> cl(a, n, vec);
+
+	EXPECT_FALSE(cl.foo());
 }
